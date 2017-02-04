d3bcfae0402b4ff0f666f1336a8708315bf4d0b5
Ben <ben@benkyriakou.com>
2017-02-04 16:26:08 +0000

    Added SCSS compilation for stylesheet.

--- blog.py
+++ blog.py
@@ -1,11 +1,13 @@
 import os
 from lxml import etree
+import sass
 
 xsl = etree.parse('./source/stylesheet.xsl')
 transform = etree.XSLT(xsl)
 
 listing = os.walk('./source')
 
+# Build the content from XML source.
 for item in listing:
   inpath, dirnames, filenames = item
 
@@ -33,3 +35,32 @@ for item in listing:
       print('Generating ' + outpath + '/' + name + '.html')
 
       result.write(outpath + '/' + name + '.html', pretty_print=True)
+
+# Build CSS from SCSS.
+# First get a sorted list of files
+print('Generating CSS')
+
+SCSS_BASE = './scss'
+CSS_FILE = './css/style.css'
+
+scss_files = os.listdir(SCSS_BASE)
+scss_files.sort()
+
+# Filter out anything without a .scss extension
+scss_files = [f for f in scss_files if f.endswith('.scss')]
+
+css_string = ''
+
+# Build the new compiled file
+for scss_file in scss_files:
+  print('Processing {!s}'.format(scss_file))
+
+  css_string += sass.compile(
+    filename=os.path.join(SCSS_BASE, scss_file),
+    output_style='compressed'
+  )
+
+# # Write the new CSS file
+with open(CSS_FILE, 'w', encoding='utf8') as fh:
+  print('Writing CSS to {!s}'.format(CSS_FILE))
+  fh.write(css_string)
